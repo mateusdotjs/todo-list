c1e6ea78fd5c8e2b9455ce0ab9962aff2570c3ca
Mateus <mateus.silvainfo@gmail.com>
2022-11-19 17:55:54 -0300

    added edit function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 class Todo {
     constructor(text) {
         this.text = text,
-            this.done = false
+        this.done = false
     }
 }
 
@@ -18,17 +18,19 @@ function listAll() {
     }
 
     for (i = 0; i < list.length; i++) {
+        //operações ternárias que retornam texto riscado e checkbox assinalado
         let p = list[i].done == true ? `<p id="text"><s>${list[i].text}</s></p>` : `<p id="text">${list[i].text}</p>`
         let input = list[i].done == true ? `<input type="checkbox" id="checkbox" onclick="check(${i})" checked>` : `<input type="checkbox" id="checkbox" onclick="check(${i}, this)"></input>`
         taskList.innerHTML += `<div class="to-do" id="div${i}"> 
                                 ${input}
                                 ${p}
-                                <img src="delete.svg" style="width:30px;height:30px" class="icon" onclick="deleteTask(${i})"></img> 
+                                <img src="img/edit.svg" style="width:30px;height:30px" class="icon" onclick="openModal(${i})"></img>
+                                <img src="img/delete.svg" style="width:30px;height:30px" class="icon" onclick="deleteTask(${i})"></img> 
                                  </div>`
     }
 }
 
-function add() {
+function addTask() {
     let task = document.querySelector('#input')
     if (task.value != '') {
         //checa se há chave database ativa no localstorage
@@ -46,10 +48,31 @@ function add() {
     }
 }
 
-function deleteTask(index) {
-    list.splice(index, 1)
-    localStorage.database = JSON.stringify(list)
-    listAll()
+function openModal(index) {
+    let modal = document.querySelector('.modal-bg')
+    modal.style.display = 'flex'
+
+    modal.onclick = e => {
+        let btnEdit = document.querySelector('#edit')
+        let btnExit = document.querySelector('#exit')
+        if (e.target == btnEdit) {
+            let inputEdit = document.querySelector('#inputEdit')
+            list[index].text = inputEdit.value
+            inputEdit.value = ''
+            modal.style.display = 'none'
+            localStorage.database = JSON.stringify(list)
+            listAll()
+        } else if (e.target == btnExit) {
+            inputEdit.value = ''
+            modal.style.display = 'none'
+        }
+    }
+}
+
+
+function closeModal() {
+    let modal = document.querySelector('.modal-bg')
+    modal.style.display = 'none'
 }
 
 
@@ -63,6 +86,12 @@ function check(index) {
 
     localStorage.database = JSON.stringify(list);
     listAll();
+}
+
+function deleteTask(index) {
+    list.splice(index, 1)
+    localStorage.database = JSON.stringify(list)
+    listAll()
 }
 
 function clearAll() {
